1f5fdf5e44e069ccb75656d81af269de98e82160
Nathan Borchelt <nathan.borchelt@evsck12.com>
2020-10-12 13:55:15 -0500

    10122020

--- CSA/Unit3/ArrayList/webstoreChecker/BorcheltWebstoreInfo.java
+++ CSA/Unit3/ArrayList/webstoreChecker/BorcheltWebstoreInfo.java
@@ -5,8 +5,10 @@ import java.util.ArrayList;
 
 public class BorcheltWebstoreInfo {
     static SnackMenu snackMenu = new SnackMenu();
-    static ArrayList<Snack> snacks = snackMenu.getSnacks()
+    static ArrayList<Snack> snacks = snackMenu.getSnacks();
     public static void main(String[] args) {
-        
+        for(Snack s:snacks){
+            System.out.println(s.getName+ " " +s.)
+        }
     }
 }
