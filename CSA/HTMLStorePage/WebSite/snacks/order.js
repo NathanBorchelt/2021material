7ad8b935efe10832c257164a07a1539f871068f1
Nathan Borchelt <nathan.d.borchelt@gmail.com>
2021-02-26 14:12:41 -0600

    worked on scripting, due tonight

--- CSA/HTMLStorePage/WebSite/snacks/order.js
+++ CSA/HTMLStorePage/WebSite/snacks/order.js
@@ -1,12 +1,45 @@
-function addToOrder(srtName,fullName,quantityId){
-    localStorage.setItem("itemsInOrder",localStorage.getItem("itemsInOrder")+" "+srtName)
-    var itemList = fullName+" "+document.getElementById("priceOfItem")+" "+document.getElementById(quantityId)
-    localStorage.setItem(srtName,itemList)
-
-}
-
 function pageName(){
     var path = window.location.pathname
     var page = path.split("/").pop()
     return page
+}
+
+function addToOrder(fullName,quantityId,costId){
+    var allStr = "itemsInOrder"
+    var numberBuying = document.getElementById(quantityId).value
+    var pageNameVar = pageName()
+    var itemList = fullName+" "+document.getElementById(costId).innerHTML+" "+numberBuying
+    try{
+        console.log("reasonable etry")
+        if(localStorage.getItem(allStr).includes(pageNameVar)){
+            console.log("tryif")
+            var addedItem = localStorage.getItem(pageNameVar)
+            console.log(addedItem)
+            var itemSplit = addedItem.split(" ")
+            console.log(itemSplit)
+            itemSplit[2] = Number(itemSplit[2])+Number(numberBuying)
+            itemList = itemSplit[0]+" "+itemSplit[1]+" "+itemSplit[2]
+            console.log(itemSplit[0]+" "+itemSplit[1]+" "+itemSplit[2])            
+        }
+        else{
+            console.log("tryelse")
+            
+            //
+            
+            localStorage.setItem(allStr,localStorage.getItem(allStr)+" "+pageNameVar)
+        }
+    }
+    catch(error){
+        //console.error(error)
+        localStorage.setItem(allStr,pageNameVar)
+        console.log("reasonable error")
+    }    
+    //console.log("addes item"+pageName())
+    localStorage.setItem(pageNameVar,itemList)
+    console.log(localStorage)
+}
+
+
+function clrMem(){
+    localStorage.clear()
 }
