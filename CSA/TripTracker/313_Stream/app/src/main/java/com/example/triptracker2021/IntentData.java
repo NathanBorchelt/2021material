6b07027bb6c6b35650f8669363db300885937dad
Nathan Borchelt <nathan.d.borchelt@gmail.com>
2021-03-16 12:25:58 -0500

    checking to see if this actually turn the files from red

---
+++ CSA/TripTracker/313_Stream/app/src/main/java/com/example/triptracker2021/IntentData.java
@@ -0,0 +1,11 @@
+package com.example.triptracker2021;
+
+public interface IntentData {
+    String EXTRA_TRIP_ID = "com.example.triptracker2021.TRIP_ID";
+    String EXTRA_TRIP_NAME = "com.example.triptracker2021.TRIP_NAME";
+    String EXTRA_TRIP_DESC = "com.example.triptracker2021.TRIP_DESC";
+    String EXTRA_TRIP_START_DATE = "com.example.triptracker2021.TRIP_START_DATE";
+    String EXTRA_TRIP_END_DATE = "com.example.triptracker2021.TRIP_END_DATE";
+    String EXTRA_TRIP_PUBLIC = "com.example.triptracker2021.TRIP_PUBLIC";
+    String EXTRA_TRIP_PUBLIC_VIEW = "com.example.triptracker2021.TRIP_PUBLIC_VIEW";
+}
