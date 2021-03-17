27b6d8d185b6ab5dbe7b62bc7b0e2aa11740a1c1
Nathan Borchelt <nathan.d.borchelt@gmail.com>
2021-03-17 14:02:29 -0500

    worked on user only

---
+++ CSA/TripTracker/317_Search/app/src/main/java/com/example/triptracker2021/IntentData.java
@@ -0,0 +1,12 @@
+package com.example.triptracker2021;
+
+public interface IntentData {
+    String EXTRA_TRIP_ID = "com.example.triptracker2021.TRIP_ID";
+    String EXTRA_TRIP_NAME = "com.example.triptracker2021.TRIP_NAME";
+    String EXTRA_TRIP_DESC = "com.example.triptracker2021.TRIP_DESC";
+    String EXTRA_TRIP_START_DATE = "com.example.triptracker2021.TRIP_START_DATE";
+    String EXTRA_TRIP_END_DATE = "com.example.triptracker2021.TRIP_END_DATE";
+    String EXTRA_TRIP_PUBLIC = "com.example.triptracker2021.TRIP_PUBLIC";
+    String EXTRA_TRIP_PUBLIC_VIEW = "com.example.triptracker2021.TRIP_PUBLIC_VIEW";
+    String EXTRA_TRIP_CREATOR = "com.example.triptracker2021.EXTRA_TRIP_CREATOR";
+}
